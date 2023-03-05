f7efc3bb4cf616a4d0bca83385623782d9b86338
anitasjohn <113326608+anitasjohn@users.noreply.github.com>
2023-03-05 17:12:33 -0600

    hmmmmmm

--- src/Main.js
+++ src/Main.js
@@ -3,6 +3,15 @@ function Main(){
         <div className="app-main">
             <div className="app-main-message"> Select a note, or create one</div>
             <div className="app-main-line"></div>
+
+
+            <div className="main-note-edit">
+                <input type="text" id="title" autoFocus/>
+                <textarea id="body" placeholder="Your Note Here"></textarea>
+
+
+
+            </div>
             
  
                        
