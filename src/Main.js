d3c2022e5ec0c90b05643b354f27f8230023f9a6
anitasjohn <113326608+anitasjohn@users.noreply.github.com>
2023-03-07 22:57:50 -0600

    some sort of thing

--- src/Main.js
+++ src/Main.js
@@ -8,12 +8,41 @@ const Main = ({ activeNote, onUpdateNote, onDeleteNote }) => {
 
   const onEditField = (field, value) => {
     onUpdateNote({
-      ...activeNote,
-      [field]: value,
-      lastModified: Date.now(),
+    ...activeNote,
+    [field]: value,
+    lastModified: Date.now(),
     });
-  };
+    };
 
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      };
+      
+      const formatDate = (when) => {
+        const date = new Date(when);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, "0");
+        const day = String(date.getDate()).padStart(2, "0");
+        return `${year}-${month}-${day}`;
+      };
+      
+      const onCalendarDateChange = (e) => {
+        const newDate = new Date(e.target.value);
+        const updatedNote = {
+          ...activeNote,
+          lastModified: newDate.getTime(),
+        };
+        onUpdateNote(updatedNote);
+      };
+      
+      
+      
+      
+      
   const onDeleteClick = () => {
     const shouldDelete = window.confirm("Are you sure you want to delete this note?");
     if (shouldDelete) {
@@ -58,6 +87,15 @@ const Main = ({ activeNote, onUpdateNote, onDeleteNote }) => {
           }}
         />
       </div>
+      <div className="app-main-note-date-picker">
+<label htmlFor="date-picker">Last Modified:</label>
+<input
+type="date"
+id="date-picker"
+value={formatDate(activeNote.lastModified)}
+onChange={(e) => onCalendarDateChange(new Date(e.target.value))}
+/>
+</div>
       <div className="app-main-note-delete">
         <button onClick={onDeleteClick}>Delete</button>
       </div>
